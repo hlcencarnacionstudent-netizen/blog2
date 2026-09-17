da8b5849e0d04f370016f1f48aead0d46593d84c
HURLEY LAWREESE C. ENCARNACION <hlcencarnacion.student@ua.edu.ph>
2026-09-17 12:53:45 +0800

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,82 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const name = "HURLEY LAWREESE C. ENCARNACION";
+
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/images/profile.jpg" />
+
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=${encodeURIComponent(
+            "https://nextjs.org/static/nextjs-logo.svg"
+          )}`}
+        />
+
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt="Profile picture"
+            />
+
+            <h1 className={utilStyles.heading2Xl}>
+              {name}
+            </h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt="Profile picture"
+              />
+            </Link>
+
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+
+      <main>{children}</main>
+
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
